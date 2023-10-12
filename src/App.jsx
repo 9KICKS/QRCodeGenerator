f052e73c018fc81715b05ef475a5afbdf9ea5ccd
Omisakin Joshua <officialjoshua9@gmail.com>
2023-10-12 18:08:10 +0100

    "updated App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,48 @@ import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
-  const [qrValue, setQRValue] = useState('');
-  const [preValue, setPreValue] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+    const [qrValue, setQRValue] = useState('');
+    const [preValue, setPreValue] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [showQRCode, setShowQRCode] = useState(false);
 
-  const generateQRCode = () => {
-    if (!qrValue || preValue === qrValue) return;
-    setPreValue(qrValue);
-    setIsLoading(true);
+    const generateQRCode = () => {
+        if (!qrValue || preValue === qrValue) return;
+        setPreValue(qrValue);
+        setIsLoading(true);
 
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  };
+        setTimeout(() => {
+            setIsLoading(false);
+            setShowQRCode(true);
+        }, 2000);
+    };
 
-  const handleInputChange = (e) => {
-    setQRValue(e.target.value.trim());
-    if (!e.target.value.trim()) {
-      setPreValue('');
-    }
-  };
+    const handleInputChange = (e) => {
+        setQRValue(e.target.value.trim());
+        if (!e.target.value.trim()) {
+            setPreValue('');
+        }
+    };
 
-  return (
-      <div className="wrapper">
-        <header>
-          <h1>QR Code Generator</h1>
-          <p>Paste a URL or enter text to create QR code</p>
-        </header>
-        <div className="form">
-          <input
-              type="text"
-              spellCheck="false"
-              placeholder="Enter text or URL"
-              value={qrValue}
-              onChange={handleInputChange}
-          />
-          <button onClick={generateQRCode}>
-            {isLoading ? 'Generating QR Code...' : 'Generate QR Code'}
-          </button>
+    return (
+        <div className="wrapper">
+            <header>
+                <h1>QR Code Generator</h1>
+                <p>Paste a URL or enter text to create QR code</p>
+            </header>
+            <div className="form">
+                <input type="text" spellCheck="false" placeholder="Enter text or URL" value={qrValue} onChange={handleInputChange}/>
+                <button onClick={generateQRCode}>
+                    {isLoading ? 'Generating QR Code...' : 'Generate QR Code'}
+                </button>
+            </div>
+            {showQRCode && (
+                <div className="qr-code">
+                    <img src={`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`} alt="qr-code" />
+                </div>
+            )}
         </div>
-        <div className="qr-code">
-          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`} alt="qr-code" />
-        </div>
-      </div>
-  );
+    );
 }
 
 export default App;
